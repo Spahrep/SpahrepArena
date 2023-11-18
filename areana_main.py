81470d6de046abddea6aeec9df4e3161f656d6c1
Spahrep <spahrep@gmail.com>
2023-11-18 15:36:05 -0400

    Logging In
    updates to get logging in to work and salted hashed passwords

--- areana_main.py
+++ areana_main.py
@@ -1,23 +1,89 @@
+# IMPORTS ETC
 import mysql.connector
-from config import host, user, password, database
-print("hello")
+import hashlib
 
-mydb = mysql.connector.connect(
-    host = host,
-    user = user,
-    password = password,
-    database = database
-    )
+#define a global variable for debugging
+debug_on = True
 
-mycursor = mydb.cursor()
+def debug(string):
+    if(debug_on):
+        print (string)
+    else:
+        pass
+try:    
+    from config import host, user, password, database
 
-mycursor.execute("Select * from test_table")
+    mydb = mysql.connector.connect(
+        host = host,
+        user = user,
+        password = password,
+        database = database
+        )
 
-myresult = mycursor.fetchall()
+    mycursor = mydb.cursor()
+except Exceptoin as e:
+    print("Error connecting to DB: "+e)
+    
+sqlString = ""
+salt=""
+userFound = False
+userAuth = False
+userID = 0;
 
-for x in myresult:
-    print(x)
-##
-    import mysql.connector
+while not userAuth:
+    userName = input("Please type your user name:")
+    password = input("Please type your password:")
+    sqlString = "Select Salt from user_table where userName = %s and isActivated = TRUE"
+    mycursor.execute(sqlString,(userName,))
+    myresult = mycursor.fetchall()
 
-print("hello")
+    # Check if there is at least one result
+    if myresult:
+        # Extract the userID from the first row
+        salt = myresult[0][0]
+        debug("ID Found, Salt Validated: ")
+        debug(salt)
+        userFound = True;
+        sqlString = "Select userID from user_table where HashedPass = %s and user_table.userName = %s"     
+       
+        password = salt + password
+        sha256_hash = hashlib.sha256()
+        sha256_hash.update(password.encode())
+        hash_hex = sha256_hash.hexdigest()
+        debug(hash_hex)
+        try:
+            mycursor.execute(sqlString,(hash_hex,userName))
+            myresult = mycursor.fetchall()
+        except Exceptoin as e:
+            print("Database Error: "+e)
+        
+        #Check if there is at least one result
+        if myresult:
+            # Extract the userID from the first row
+            userID_id = myresult[0][0]
+            debug("userID ID Found, Password Validated: "+str(userID_id))
+            userAuth = True
+            print("User Validated, preparing the adventure...")
+            #update last log in
+            proc_name = "UpdateLastLoginTime"
+            mycursor.callproc(proc_name,(userID_id,))
+            mydb.commit()
+        else:
+            # Handle the case where no matching records are found
+            userID_id = None
+            debug("Could Not Authenticate")
+
+
+
+        
+    else:
+        # Handle the case where no matching records are found
+        userID_id = None
+        continue
+#We are now in and Authenticated
+     
+
+
+mycursor.close()
+mydb.close()
+    
